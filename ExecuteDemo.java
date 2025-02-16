39b656504f76a29cb044dfd675cfb4cec7900b32
Umberto Paroli <buparoli9@gmail.com>
2025-02-16 16:43:20 +0100

    create due metodi dummy per far parlare i thread

--- ExecuteDemo.java
+++ ExecuteDemo.java
@@ -1,6 +1,17 @@
 public class ExecuteDemo {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        TournamentNode clientNode = new TournamentNode("localhost", 3000);
+        TournamentNode serverNode = new TournamentNode("localhost", 3000);
+
+        Thread serverThread = new Thread(() -> {
+            serverNode.serverCreateFile(null);
+        });
+        Thread clientThread = new Thread(() -> {
+            clientNode.clientCreateFile(null);
+        });
+
+        serverThread.start();
+        clientThread.start();
     }
     
 }
--- TournamentNode.java
+++ TournamentNode.java
@@ -1,29 +1,69 @@
+import java.io.*;
+import java.net.*;
+
 public class TournamentNode {
 
-    //metodi richiesti
-    //creazione file
-    public void createFile(String name) {
-        //TODO
+    private String ip_address;
+    private int port;
+
+    //costruttore
+    public TournamentNode(String ip_address, int port) {
+        this.ip_address = ip_address;
+        this.port = port;
+    }   
+
+    public void clientCreateFile(String path){ 
+        try {
+            Socket s = new Socket("localhost", 3000);
+            ObjectOutputStream oos = new ObjectOutputStream(s.getOutputStream());
+            //invio messaggio
+            oos.writeObject("CIAO coestai?");
+            oos.flush();
+            //ricezione risposta
+            ObjectInputStream ois = new ObjectInputStream(s.getInputStream());
+            String result = (String)ois.readObject();
+            System.out.println("Client ricevuto: "+result);
+
+            s.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
     }
-    //cabcellazione file
-    public void deleteFile(String name) {
-        //TODO
+
+    public void serverCreateFile(String path){
+        try {
+            ServerSocket ss;
+            Socket clientSocket;
+
+            //stabilire connessione
+            ss = new ServerSocket(3000);
+            System.out.println("Server avviato e in attesa...");
+            clientSocket = ss.accept();
+
+            System.out.print("Server ha ricevuto una richiesta: ");
+            ObjectInputStream ois = new ObjectInputStream(clientSocket.getInputStream());
+            String par = (String)ois.readObject();
+            System.out.println(par);
+            StringBuilder rev = new StringBuilder(par).reverse();
+            
+            String result = rev.toString();
+            ObjectOutputStream oos = new ObjectOutputStream(clientSocket.getOutputStream());
+
+            System.out.println("Server invia " +result);
+            oos.writeObject(result);
+            oos.flush();
+            System.out.println("Server fatto!");
+            
+            clientSocket.close();
+            ss.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
     }
-    //Open file
-    public void openFile(String name) {
-        //TODO
-    }
-    //Close file
-    public void closeFile(String name) {
-        //TODO
-    }
-    //Read file
-    public void readFile(String name) {
-        //TODO
-    }
-    //Write file
-    public void writeFile(String name) {
-        //TODO
-    }
+    /*Metodi richiesti: create/delete, Open/close, Read,write ed eventualmente lock */
 
 }
