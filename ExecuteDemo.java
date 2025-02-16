244c00ff892f4eb991e59e4fdfd18f74c764f236
Umberto Paroli <buparoli9@gmail.com>
2025-02-16 17:17:41 +0100

    aggiunto metodo delete specchio di creare, da fare un refactoring per pulire il codice, eliminare le perti ripetute e far si che non serva ogni volta aprire e chiudere le porte rendendolo un processo continuo

--- ExecuteDemo.java
+++ ExecuteDemo.java
@@ -3,15 +3,40 @@ public class ExecuteDemo {
         TournamentNode serverNode = new TournamentNode("localhost", 3000);
         TournamentNode clientNode = new TournamentNode("localhost", 3001);
 
-        Thread serverThread = new Thread(() -> {
+        Thread serverCreateThread = new Thread(() -> {
             serverNode.serverCreateFile();
         });
-        Thread clientThread = new Thread(() -> {
+        Thread clientCreateThread = new Thread(() -> {
             clientNode.clientCreateFile("localhost", 3000, "file.txt");
         });
+        Thread serverDeleteThread = new Thread(() -> {
+            serverNode.serverDeleteFile();
+        });
+        Thread clientDeleteThread = new Thread(() -> {
+            clientNode.clientDeleteFile("localhost", 3000, "file.txt");
+        });
 
-        serverThread.start();
-        clientThread.start();
+        //synchronization barrier
+        try {
+            serverCreateThread.start();
+            clientCreateThread.start();
+
+            serverCreateThread.join();
+            clientCreateThread.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            serverDeleteThread.start();
+            clientDeleteThread.start();
+
+            serverDeleteThread.join();
+            clientDeleteThread.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
     }
     
 }
